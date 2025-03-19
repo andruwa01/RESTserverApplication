f94b310a15f3bf563f641473433dc7026aa2108a
andruwa_01 <cheprasov.work@gmail.com>
2025-03-19 09:02:06 +0300

    WORKS: remove bugs, test server responses

--- src/server.cpp
+++ src/server.cpp
@@ -4,6 +4,7 @@
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/strand.hpp>
 #include <boost/config.hpp>
+
 #include <iostream>
 #include <memory>
 #include <string>
@@ -14,25 +15,30 @@ namespace http = beast::http;   // from <boost/beast/http.hpp>
 namespace net = boost::asio;    // from <boost/asio.hpp>
 using tcp = net::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
-// This function produces an HTTP response for the given request.
-http::response<http::string_body> handle_request(http::request<http::string_body> const& req) {
-    // Respond to GET request with "Hello, World!"
-    if (req.method() == http::verb::get) {
+// this function produces an HTTP response for the given request.
+http::response<http::string_body> handle_request(http::request<http::string_body> const& req)
+{
+    std::cout << "generate response\n";
+
+    // response to GET request with "Hello, World!"
+    if (req.method() == http::verb::get)
+    {
         http::response<http::string_body> res{http::status::ok, req.version()};
         res.set(http::field::server, "Beast");
         res.set(http::field::content_type, "text/plain");
         res.keep_alive(req.keep_alive());
         res.body() = "Hello, World!";
-        res.prepare_payload();
+        res.prepare_payload(); // setting content-length in the header
         return res;
     }
 
-    // Default response for unsupported methods
+    // default response for unsupported methods
     return http::response<http::string_body>{http::status::bad_request, req.version()};
 }
 
-// This class handles an HTTP server connection.
-class Session : public std::enable_shared_from_this<Session> {
+// this class handles an HTTP server connection.
+class Session : public std::enable_shared_from_this<Session>
+{
     tcp::socket socket_;
     beast::flat_buffer buffer_;
     http::request<http::string_body> req_;
@@ -40,37 +46,46 @@ class Session : public std::enable_shared_from_this<Session> {
 public:
     explicit Session(tcp::socket socket) : socket_(std::move(socket)) {}
 
-    void run() {
+    void run()
+    {
         do_read();
     }
 
 private:
-    void do_read() {
+    void do_read()
+    {
+        std::cout << "get request!\n";
+
         auto self(shared_from_this());
-        http::async_read(socket_, buffer_, req_, [this, self](beast::error_code ec, std::size_t) {
-            if (!ec) {
+        http::async_read(socket_, buffer_, req_, [this, self](beast::error_code ec, std::size_t)
+        {
+            if (!ec)
+            {
                 do_write(handle_request(req_));
             }
         });
     }
 
-    void do_write(http::response<http::string_body> res) {
+    void do_write(http::response<http::string_body> res)
+    {
         auto self(shared_from_this());
         auto sp = std::make_shared<http::response<http::string_body>>(std::move(res));
-        http::async_write(socket_, *sp, [this, self, sp](beast::error_code ec, std::size_t) {
+        http::async_write(socket_, *sp, [this, self, sp](beast::error_code ec, std::size_t)
+        {
             socket_.shutdown(tcp::socket::shutdown_send, ec);
         });
     }
 };
 
-// This class accepts incoming connections and launches the sessions.
-class Listener : public std::enable_shared_from_this<Listener> {
+// this class accepts incoming connections and launches the sessions.
+class Listener : public std::enable_shared_from_this<Listener>
+{
     net::io_context& ioc_;
     tcp::acceptor acceptor_;
 
 public:
-    Listener(net::io_context& ioc, tcp::endpoint endpoint)
-        : ioc_(ioc), acceptor_(net::make_strand(ioc)) {
+    Listener(net::io_context& ioc, tcp::endpoint endpoint) : ioc_(ioc), acceptor_(net::make_strand(ioc))
+    {
         beast::error_code ec;
 
         // Open the acceptor
@@ -101,31 +116,57 @@ public:
             return;
         }
 
+    }
+
+    void run()
+    {
         do_accept();
     }
 
 private:
-    void do_accept() {
-        acceptor_.async_accept(net::make_strand(ioc_), [this](beast::error_code ec, tcp::socket socket) {
-            if (!ec) {
-                std::make_shared<Session>(std::move(socket))->run();
+    // void do_accept() {
+    //     acceptor_.async_accept(net::make_strand(ioc_), [this](beast::error_code ec, tcp::socket socket) {
+    //         if (!ec) {
+    //             // std::make_shared<Session>(std::move(socket))->run();
+    //             auto session = std::make_shared<Session>(std::move(socket));
+    //             session->run();
+    //         }
+    //         do_accept();
+    //     });
+    // }
+    void do_accept()
+    {
+        acceptor_.async_accept(net::make_strand(ioc_), [self = weak_from_this()](beast::error_code ec, tcp::socket socket)
+        {
+            if (auto shared_self = self.lock()) // checking existing of Listener object 
+            { 
+                if (!ec)
+                {
+                    auto session = std::make_shared<Session>(std::move(socket));
+                    session->run();
+                }
+                shared_self->do_accept();
             }
-            do_accept();
         });
     }
 };
 
 int main() {
-    try {
+    try
+    {
         auto const address = net::ip::make_address("0.0.0.0");
         unsigned short port = 8080;
 
         net::io_context ioc{1};
 
-        std::make_shared<Listener>(ioc, tcp::endpoint{address, port})->run();
+        // std::make_shared<Listener>(ioc, tcp::endpoint{address, port})->run();
+        auto listener = std::make_shared<Listener>(ioc, tcp::endpoint{address, port});
+        listener->run();
 
         ioc.run();
-    } catch (const std::exception& e) {
+    }
+    catch (const std::exception& e)
+    {
         std::cerr << "Error: " << e.what() << std::endl;
     }
 }
