514f1665ddb07197b868873597c1601f76e5e462
andruwa_01 <cheprasov.work@gmail.com>
2025-03-19 13:09:18 +0300

    add get , post , put , delete methods to handle

--- src/server.cpp
+++ src/server.cpp
@@ -22,16 +22,54 @@ http::response<http::string_body> handle_request(http::request<http::string_body
 {
     if (req.method() == http::verb::get && req.target() == "/api/data")
     {
-        // handle get request
-        
+        // handle GET request
         nlohmann::json json_response = {{"message", "this is a GET request"}}; 
-
         http::response<http::string_body> res{http::status::ok, req.version()};
         res.set(http::field::server, "Beast");
-        res.set(http::field::content_type, "text/plain");
+        res.set(http::field::content_type, "application/json");
         res.keep_alive(req.keep_alive());
         res.body() = json_response.dump();
-        res.prepare_payload(); // setting content-length in the header
+        res.prepare_payload();
+        return res;
+    }
+    else if (req.method() == http::verb::post && req.target() == "/api/data")
+    {
+        // Handle POST request
+        auto json_request = nlohmann::json::parse(req.body());
+        std::string response_message = "Received: " + json_request.dump();
+        nlohmann::json json_response = {{"message", response_message}};
+        http::response<http::string_body> res{http::status::ok, req.version()};
+        res.set(http::field::server, "Beast");
+        res.set(http::field::content_type, "application/json");
+        res.keep_alive(req.keep_alive());
+        res.body() = json_response.dump();
+        res.prepare_payload();
+        return res;
+    }
+    else if (req.method() == http::verb::put && req.target() == "/api/data")
+    {
+        // Handle PUT request
+        auto json_request = nlohmann::json::parse(req.body());
+        std::string response_message = "Updated: " + json_request.dump();
+        nlohmann::json json_response = {{"message", response_message}};
+        http::response<http::string_body> res{http::status::ok, req.version()};
+        res.set(http::field::server, "Beast");
+        res.set(http::field::content_type, "application/json");
+        res.keep_alive(req.keep_alive());
+        res.body() = json_response.dump();
+        res.prepare_payload();
+        return res;
+    }
+    else if (req.method() == http::verb::delete_ && req.target() == "/api/data")
+    {
+        // Handle DELETE request
+        nlohmann::json json_response = {{"message", "Resource deleted"}};
+        http::response<http::string_body> res{http::status::ok, req.version()};
+        res.set(http::field::server, "Beast");
+        res.set(http::field::content_type, "application/json");
+        res.keep_alive(req.keep_alive());
+        res.body() = json_response.dump();
+        res.prepare_payload();
         return res;
     }
 
